4c30e3eb9d4b67ce096edbdb0e80dbe68006494a
Muhammad Agung Rizkyana <muhammadagungrizkyana@Muhammads-MacBook-Pro.local>
2024-12-13 17:36:03 +0700

    feat: add new CRUD category

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,6 +5,7 @@ import UserModel from "../models/user.model";
 import { encrypt } from "../utils/encryption";
 import { generateToken } from "../utils/jwt";
 import { IReqUser } from "../utils/interfaces";
+import response from "../utils/response";
 
 type TRegister = {
   fullName: string;
@@ -77,16 +78,9 @@ export default {
         password,
       });
 
-      res.status(200).json({
-        message: "Success registration!",
-        data: result,
-      });
+      response.success(res, result, "success registration!");
     } catch (error) {
-      const err = error as unknown as Error;
-      res.status(400).json({
-        message: err.message,
-        data: null,
-      });
+      response.error(res, error, "failed registration");
     }
   },
   async login(req: Request, res: Response) {
@@ -115,10 +109,7 @@ export default {
       });
 
       if (!userByIdentifier) {
-        return res.status(403).json({
-          message: "user not found",
-          data: null,
-        });
+        return response.unauthorized(res, "user not found");
       }
 
       // validasi password
@@ -126,10 +117,7 @@ export default {
         encrypt(password) === userByIdentifier.password;
 
       if (!validatePassword) {
-        return res.status(403).json({
-          message: "user not found",
-          data: null,
-        });
+        return response.unauthorized(res, "user not found");
       }
 
       const token = generateToken({
@@ -137,16 +125,9 @@ export default {
         role: userByIdentifier.role,
       });
 
-      res.status(200).json({
-        message: "Login success",
-        data: token,
-      });
+      response.success(res, token, "login success");
     } catch (error) {
-      const err = error as unknown as Error;
-      res.status(400).json({
-        message: err.message,
-        data: null,
-      });
+      response.error(res, error, "login failed");
     }
   },
   async me(req: IReqUser, res: Response) {
@@ -160,16 +141,9 @@ export default {
       const user = req.user;
       const result = await UserModel.findById(user?.id);
 
-      res.status(200).json({
-        message: "Success get user profile",
-        data: result,
-      });
+      response.success(res, result, "success get user profile");
     } catch (error) {
-      const err = error as unknown as Error;
-      res.status(400).json({
-        message: err.message,
-        data: null,
-      });
+      response.error(res, error, "failed get user profile");
     }
   },
   async activation(req: Request, res: Response) {
@@ -194,16 +168,10 @@ export default {
           new: true,
         }
       );
-      res.status(200).json({
-        message: "user successfully activated",
-        data: user,
-      });
+
+      response.success(res, user, "user successfully activated");
     } catch (error) {
-      const err = error as unknown as Error;
-      res.status(400).json({
-        message: err.message,
-        data: null,
-      });
+      response.error(res, error, "user is failed activated");
     }
   },
 };
--- src/controllers/media.controller.ts
+++ src/controllers/media.controller.ts
@@ -1,67 +1,46 @@
 import { Response } from "express";
 import { IReqUser } from "../utils/interfaces";
 import uploader from "../utils/uploader";
+import response from "../utils/response";
 
 export default {
   async single(req: IReqUser, res: Response) {
     if (!req.file) {
-      return res.status(400).json({
-        data: null,
-        message: "File is not exist",
-      });
+      return response.error(res, null, "file not found");
     }
 
     try {
       const result = await uploader.uploadSingle(
         req.file as Express.Multer.File
       );
-      res.status(200).json({
-        data: result,
-        message: "success upload a file",
-      });
+      response.success(res, result, "success upload a file");
     } catch {
-      res.status(500).json({
-        data: null,
-        message: "failed upload a file",
-      });
+      response.error(res, null, "failed upload a file");
     }
   },
   async multiple(req: IReqUser, res: Response) {
     if (!req.files || req.files.length === 0) {
-      return res.status(400).json({
-        data: null,
-        message: "Files are not exist",
-      });
+      return response.error(res, null, "files not found");
     }
 
     try {
       const result = await uploader.uploadMultiple(
         req.files as Express.Multer.File[]
       );
-      res.status(200).json({
-        data: result,
-        message: "success upload files",
-      });
+
+      response.success(res, result, "success upload files");
     } catch {
-      res.status(500).json({
-        data: null,
-        message: "failed upload files",
-      });
+      response.error(res, null, "failed upload files");
     }
   },
   async remove(req: IReqUser, res: Response) {
     try {
       const { fileUrl } = req.body as { fileUrl: string };
       const result = await uploader.remove(fileUrl);
-      res.status(200).json({
-        data: result,
-        message: "success remove file",
-      });
+
+      response.success(res, result, "success remove file");
     } catch {
-      res.status(500).json({
-        data: null,
-        message: "failed remove file",
-      });
+      response.error(res, null, "failed remove file");
     }
   },
 };
