315a3328810f91b03907d87541403658b6eee0a1
wpucourse <wpucourse@gmail.com>
2024-12-30 05:52:21 +0700

    fix: delete media when delete event

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -3,6 +3,7 @@ import { IPaginationQuery, IReqUser } from "../utils/interfaces";
 import response from "../utils/response";
 import EventModel, { eventDTO, TypeEvent } from "../models/event.model";
 import { FilterQuery, isValidObjectId } from "mongoose";
+import uploader from "../utils/uploader";
 
 export default {
   async create(req: IReqUser, res: Response) {
@@ -94,6 +95,9 @@ export default {
       const result = await EventModel.findByIdAndUpdate(id, req.body, {
         new: true,
       });
+
+      if (!result) return response.notFound(res, "event not found");
+
       response.success(res, result, "success update an event");
     } catch (error) {
       response.error(res, error, "failed update an event");
@@ -105,6 +109,11 @@ export default {
       const result = await EventModel.findByIdAndDelete(id, {
         new: true,
       });
+
+      if (!result) return response.notFound(res, "event not found");
+
+      await uploader.remove(result.banner);
+
       response.success(res, result, "success remove an event");
     } catch (error) {
       response.error(res, error, "failed to remove an event");
@@ -116,6 +125,9 @@ export default {
       const result = await EventModel.findOne({
         slug,
       });
+
+      if (!result) return response.notFound(res, "event not found");
+
       response.success(res, result, "success find one by slug an event");
     } catch (error) {
       response.error(res, error, "failed find one by slug an event");
