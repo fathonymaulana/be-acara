f099d4b018d3a780518553bb42d62940e808b527
wpucourse <wpucourse@gmail.com>
2024-12-29 13:50:50 +0700

    feat: remove order

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -104,6 +104,18 @@ router.get(
   */
 );
 
+router.delete(
+  "/orders/:orderId",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN])],
+  orderController.remove
+  /*
+  #swagger.tags = ['Order']
+  #swagger.security = [{
+    "bearerAuth": ""
+  }]
+  */
+);
+
 router.post(
   "/banners",
   [authMiddleware, aclMiddleware([ROLES.ADMIN])],
